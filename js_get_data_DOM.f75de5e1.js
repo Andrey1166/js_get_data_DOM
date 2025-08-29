5e73735f217f132c2ce147e536f2952d23a4799e
Andrey1166 <aannddrreeyy266@gmail.com>
2025-08-30 02:37:00 +0300

    make build

---
+++ js_get_data_DOM.f75de5e1.js
@@ -0,0 +1,18 @@
+'use strict';
+const populationList = document.querySelectorAll('.population');
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+let total = 0;
+let validCount = 0;
+for (const item of populationList){
+    const num = item.textContent.replaceAll(',', '').trim();
+    if (Number.isFinite(Number(num))) {
+        total += +num;
+        validCount++;
+    }
+}
+const separator = populationList[0].textContent.split('').filter((item)=>!Number(item))[0];
+totalPopulation.textContent = total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, separator) || 0;
+averagePopulation.textContent = Math.round(total / validCount).toString().replace(/\B(?=(\d{3})+(?!\d))/g, separator) || 0;
+
+//# sourceMappingURL=js_get_data_DOM.f75de5e1.js.map
